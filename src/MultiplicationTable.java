45574800c5f7a587aab06f60815773cb4a8d9ee4
DhaswanthB <dhaswanth13012008@gmail.com>
2026-03-17 14:00:01 +0530

    Coded UC2 to Print OOPS to Console

--- src/MultiplicationTable.java
+++ src/MultiplicationTable.java
@@ -1,2 +1,26 @@
-public class sde {
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+
+        int[] multiplicationResult = new int[10];
+
+        for (int i = 1; i <= 10; i++) {
+            multiplicationResult[i - 1] = number * i;
+        }
+
+
+        System.out.println("\nMultiplication Table of " + number + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " * " + i + " = " + multiplicationResult[i - 1]);
+        }
+
+        sc.close();
+    }
 }
